a436e05f9a7a355bf1da00b5a10d405ea97b9887
ayethan <hainthantkyaw.com>
2017-12-20 12:26:12 +0630

    signup & login screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,38 @@
 @endsection
 
 @section('content')
-  This is my content!
+  <div class="row">
+      <div class="col-md-6">
+      <h3>Sign Up</h3>
+          <form action="" method="post">
+              <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" name="email" class="form-control" id="email">
+              </div>
+              <div class="form-group">
+                  <label for="first_name">Your first name</label>
+                  <input type="text" name="first_name" class="form-control" id="first_name">
+              </div>
+              <div class="form-group">
+                  <label for="password">Your Password</label>
+                  <input type="password" name="password" class="form-control" id="password">
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+      </div>
+       <div class="col-md-6">
+       <h3>Sign In</h3>
+          <form action="" method="post">
+              <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" name="email" class="form-control" id="email">
+              </div>
+              
+              <div class="form-group">
+                  <label for="password">Your Password</label>
+                  <input type="password" name="password" class="form-control" id="password">
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+  </div>
 @endsection
