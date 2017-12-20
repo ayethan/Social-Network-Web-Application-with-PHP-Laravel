6bd832f4fb7df466cd6c225e7a617b8831cb7c89
ayethan <hainthantkyaw.com>
2017-12-20 22:32:00 +0630

    logging user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
   <div class="row">
       <div class="col-md-6">
       <h3>Sign Up</h3>
-          <form action="{{route ('signup')}}" method="post">
+          <form action="{{ route ('signup') }}" method="post">
               <div class="form-group">
                   <label for="email">Email</label>
                   <input type="email" name="email" class="form-control" id="email">
@@ -26,7 +26,7 @@
       </div>
        <div class="col-md-6">
        <h3>Sign In</h3>
-          <form action="signin" method="post">
+          <form action="{{ route('signin') }}" method="post">
               <div class="form-group">
                   <label for="email">Email</label>
                   <input type="email" name="email" class="form-control" id="email">
@@ -37,6 +37,7 @@
                   <input type="password" name="password" class="form-control" id="password">
               </div>
               <button type="submit" class="btn btn-primary">Submit</button>
+              <input type="hidden" name="_token" value="{{ Session::token() }}">
               </form>
   </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,13 @@ Route::get('/', function () {
 Route::post('/signup',[
 'uses' =>'UserController@postSignUp',
  'as' =>'signup']);
+
+Route::post('/signin',[
+'uses' =>'UserController@postSignIn',
+ 'as' =>'signin']);
+
+Route::get('/dashboard',[
+'uses'=>'UserController@getDashboard',
+ 'as'=>'dashboard'
+]);
 });
