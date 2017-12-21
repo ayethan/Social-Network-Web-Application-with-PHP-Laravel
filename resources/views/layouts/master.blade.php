92c87d1deb0e86ce986cf8f10482c48eaa62a159
ayethan <hainthantkyaw.com>
2017-12-22 00:31:17 +0630

    Post Validation Outputting Errors

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<title>@yield('title')</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="{{ URL::to('css/main.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ URL::to('style/main.css')}}">
 
 </head>
 <body>
