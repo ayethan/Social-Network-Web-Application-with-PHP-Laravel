6dae3e2c23231494d4a50d9914bc8151229c9004
ayethan <hainthantkyaw.com>
2017-12-28 10:49:59 +0630

    Likes, Dislikes and Wrap Up

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,28 @@
-$('document').ready(function() {
-	var postId = 0;
-	var postBodyElement = null;
-	$('.post').find('.interaction').find('.edit').on('click', function (event) {
-		event.preventDefault();
+var postId = 0;
+var postBodyElement = null;
 
-		postBodyElement = event.target.parentNode.parentNode.childNodes[1];	
-		var postBody = postBodyElement.textContent;
-		postId = event.target.parentNode.parentNode.dataset['postid'];
-		$('#post-body').val(postBody);
-		$('#edit-modal').modal();
-	});
-	$('#modal-save').on('click',function(){
-		$.ajax({
-			method: 'POST',
-			url: urlEdi,
-			data: {body: $('#post-body').val(), postId: postId, _token: token}
-		})
-		.done(function(msg){
-			$(postBodyElement).text(msg['new_body']);
-			$('#edit-modal').modal('hide');
-		});
-	});
-	
+$('.post').find('.interaction').find('.edit').on('click', function (event) {
+    event.preventDefault();
+
+    postBodyElement = event.target.parentNode.parentNode.childNodes[1];
+    var postBody = postBodyElement.textContent;
+    postId = event.target.parentNode.parentNode.dataset['postid'];
+    $('#post-body').val(postBody);
+    $('#edit-modal').modal();
+});
+
+$('#modal-save').on('click', function () {
+    $.ajax({
+            method: 'POST',
+            url: urlEdit,
+            data: {body: $('#post-body').val(), postId: postId, _token: token}
+        })
+        .done(function (msg) {
+            $(postBodyElement).text(msg['new_body']);
+            $('#edit-modal').modal('hide');
+        });
+});
+
 $('.like').on('click', function(event) {
     event.preventDefault();
     postId = event.target.parentNode.parentNode.dataset['postid'];
@@ -31,7 +32,12 @@ $('.like').on('click', function(event) {
         url: urlLike,
         data: {isLike: isLike, postId: postId, _token: token}
     })
-     .done(function() {
-
-    });
-});	
+        .done(function() {
+            event.target.innerText = isLike ? event.target.innerText == 'Like' ? 'You like this post' : 'Like' : event.target.innerText == 'Dislike' ? 'You don\'t like this post' : 'Dislike';
+            if (isLike) {
+                event.target.nextElementSibling.innerText = 'Dislike';
+            } else {
+                event.target.previousElementSibling.innerText = 'Like';
+            }
+        });
+});
